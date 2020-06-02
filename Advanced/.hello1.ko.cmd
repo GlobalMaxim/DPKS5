b9b2a83c5cca2d395237d4ce0237c0483355a161
Verchenko Maxim <verchenko1998@gmail.com>
2020-06-02 22:06:18 +0300

    Lab5

---
+++ Advanced/.hello1.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/parallels/Desktop/Advanced/hello1.ko := ccache arm-eabi-ld -r  -EL -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds  --build-id  -o /home/parallels/Desktop/Advanced/hello1.ko /home/parallels/Desktop/Advanced/hello1.o /home/parallels/Desktop/Advanced/hello1.mod.o ;  true
